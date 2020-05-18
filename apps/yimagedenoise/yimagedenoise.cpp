cd8cefce5c6c060d6a8515bce19ffe330b945fd7
francescosassi <francesco.sassi.phys@gmail.com>
2020-05-19 01:04:13 +0200

    fixed linux build

--- apps/yimagedenoise/yimagedenoise.cpp
+++ apps/yimagedenoise/yimagedenoise.cpp
@@ -17,15 +17,15 @@ using namespace std::string_literals;
 int main(int argc, const char* argv[]) {
   // options
   auto param = ""s;
-  auto output   = "out.png"s;
-  auto filename = "img.png"s;
+  auto out_file   = "out.png"s;
+  auto in_file = "img.png"s;
   auto type = "nlmean"s;
 
   // parse command line
   auto cli = cli::make_cli("yimagedenoise", "Intel Denoiser");
   add_option(cli, "--type,-t", type, "Output image filename", true);
-  add_option(cli, "--outimage,-o", output, "Output image filename", true);
-  add_option(cli, "image", filename, "Input image filename", true);
+  add_option(cli, "--outimage,-o", out_file, "Output image filename", true);
+  add_option(cli, "image", in_file, "Input image filename", true);
   parse_cli(cli, argc, argv);
 
   if (type == "nlmean"){
@@ -37,17 +37,17 @@ int main(int argc, const char* argv[]) {
     // load
     auto img = img::image<vec4f>{};
 
-    if (!load_image(filename, img, ioerror)) cli::print_fatal(ioerror);
+    if (!load_image(in_file, img, ioerror)) cli::print_fatal(ioerror);
 
     // corrections
     img = dns::denoise_nlmean(img);
 
     // save
-    if (!save_image(output, float_to_byte(img), ioerror))
+    if (!save_image(out_file, float_to_byte(img), ioerror))
       cli::print_fatal(ioerror);
   } else {
 
-    auto buffer = oidn::loadImage("tests/cornellbox.pfm");
+    auto buffer = oidn::loadImage(in_file);
     printf("ciao\n");
     const int width  = buffer.getWidth();
     const int height = buffer.getHeight();
@@ -57,7 +57,7 @@ int main(int argc, const char* argv[]) {
     printf("%p \n", output.getData());
     //output.getData();
     dns::denoise(buffer.getData(), output.getData(), width, height);
-    oidn::saveImage("tests/test_denoised.pfm", output);
+    oidn::saveImage(out_file, output);
 
   }
 
